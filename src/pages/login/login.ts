d43b60a4f9de19bf204958c1406253c9795bc63f
danimnh <39585094+danimnh@users.noreply.github.com>
2019-05-05 07:46:55 +0700

    Add files via upload

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { MenuPage } from '../menu/menu';
 
 @Component({
   selector: 'page-login',
@@ -7,11 +9,39 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  @ViewChild("username") username;
+  @ViewChild("password") password;
+
+  constructor(private fire:AngularFireAuth, public navCtrl: NavController,public alertCtrl: AlertController, public navParams: NavParams) {
   }
 
   doLogin() {
-    this.navCtrl.setRoot('MenuPage');
+    this.navCtrl.setRoot(MenuPage);
   }
   
+  alert(message: string){
+    this.alertCtrl.create({
+    title:"Info!",
+    subTitle: message,
+    buttons: ['OK']
+    })
+    .present();
+  };
+
+    
+  signInUser(){
+    this.fire.auth.signInWithEmailAndPassword(this.username.value, this.password.value)
+    .then( data => {
+      console.log('masuk cuiii', this.fire.auth.currentUser);
+      this.alert('Logged in!');
+      this.navCtrl.setRoot( MenuPage );
+    })
+    .catch ( error => {
+      console.log('error cok');
+      this.alert('Gagal Logged in!');
+    })
+
+    console.log('would sign in with', this.username.value, this.password.value);
+  }
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { EditProfilePage } from '../editprofile/editprofile';
+import { LoadingController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 
 @Component({
   selector: 'page-register',
@@ -8,7 +11,58 @@ import { EditProfilePage } from '../editprofile/editprofile';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild("username") username;
+@ViewChild("password") password;
+
+  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController,  public loading: LoadingController) {
+  }
+
+  alert(message: string){
+    this.alertCtrl.create({
+    title:"Info!",
+    subTitle: message,
+    buttons: ['OK']
+    })
+    .present();
+  };
+
+  
+  Register(){
+    if(this.username.value==""){
+      let alert = this.alertCtrl.create({
+        title:"WADIDAW",
+        subTitle:"Kolom Username belum terisi",
+        buttons: ['OK']
+      });
+
+      alert.present();
+    }
+      else if(this.password.value==""){
+      let alert = this.alertCtrl.create({
+        title:"Haduh kamu tuh ya",
+        subTitle:"passwordnya isi",
+        buttons: ['OK']
+      });
+
+      alert.present();
+    }
+
+      else{
+         this.fire.auth.createUserWithEmailAndPassword(this.username.value, this.password.value)
+         .then(data =>{
+            console.log('got data', data);
+            this.alert('Registered');
+            this.navCtrl.push(EditProfilePage);
+         })
+
+         .catch(error => {
+            console.log('error cui', error);
+            this.alert(error.message);
+
+         })
+         console.log('would register user with ', this.username.value, this.password.value);
+      }
+
   }
 
   ionViewDidLoad() {
